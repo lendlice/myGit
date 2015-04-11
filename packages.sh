ff8ed9d1baab8ca0f0d53ebfe090c3a9915124b5
lendlice <chenren.xu@gmail.com>
2015-04-11 19:28:34 -0400

    restructure the directories

--- packages.sh
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/bash
-
-sudo apt-get install gnome-tweak-tool gnome-session-fallback
-
-sudo apt-get install wireless-tools airmon-ng hwinfo 
-#sudo apt-get install hostapd bridge-utils isc-dhcp-server 
